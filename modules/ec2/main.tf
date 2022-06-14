c6f7c7dbe134c9a446f3fd3b5e4f91a6754cc2d4
Henry S <henrylysu@gmail.com>
2022-06-14 16:35:02 -0400

    adding metadata_options block

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,7 +4,6 @@ resource "aws_instance" "main" {
   ami                         = var.ami_id
   associate_public_ip_address = false
   disable_api_termination     = var.disable_api_termination
-  http_tokens                 = required
   iam_instance_profile        = var.instance_profile
   instance_type               = var.instance_type
   key_name                    = var.ssh_key_name
@@ -13,6 +12,9 @@ resource "aws_instance" "main" {
   volume_tags                 = var.tags
   vpc_security_group_ids      = var.security_group_ids
 
+  metadata_options {
+    http_tokens = required
+  }
 
   root_block_device {
     volume_size = var.volume_size
